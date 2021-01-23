e9f43dcdb1319e5f867152b6f20d048fb5f484b1
Claudio Caseiro <ccaseiro@gmail.com>
2021-01-23 19:23:49 +0000

    Numpad layer

--- keyboards/moonlander/keymaps/ccaseiro/keymap.c
+++ keyboards/moonlander/keymaps/ccaseiro/keymap.c
@@ -22,10 +22,10 @@
 #include "version.h"
 
 enum layers {
-    DVORAK, // Dvorak
-    GAME,   // default layer
-    SYMB,   // symbols
-    MDIA,   // media keys
+    DVORAK,
+    GAME,
+    NUMPAD,
+    MDIA,
     SWITCH,
 };
 
@@ -41,6 +41,8 @@ enum custom_keycodes {
 #define TO_DVRK TO(DVORAK)
 #define TO_GAME TO(GAME)
 
+#define SPC_NUM LT(NUMPAD, KC_SPC)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DVORAK] = LAYOUT_moonlander(
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CTL_ESC, HOME_A,  HOME_O,  HOME_E,  HOME_U,  KC_I,    KC_HYPR,           KC_MEH,  KC_D,    HOME_H,  HOME_T,  HOME_N,  HOME_S,  KC_MINS,
         OSM_SFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_CAPS,
         MO_SWIT, KC_LGUI, KC_LALT, KC_LALT, KC_ESC,           XXXXXXX,           XXXXXXX,          KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, MO_SWIT,
-                                            KC_SPC,  KC_TAB,  KC_ENT,            XXXXXXX, KC_ENT,  KC_BSPC
+                                            SPC_NUM, KC_TAB,  KC_ENT,            XXXXXXX, KC_ENT,  KC_BSPC
     ),
 
      [GAME] = LAYOUT_moonlander(
@@ -61,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_SPC,  KC_TAB,  KC_ENT,            _______, _______, _______
     ),
 
-    [SYMB] = LAYOUT_moonlander(
-        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,           _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,           _______, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______,           _______, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-        EEP_RST, _______, _______, _______, _______,          RGB_VAI,           RGB_TOG,          _______, KC_DOT,  KC_0,    KC_EQL,  _______,
-                                            RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,_______, _______
+    [NUMPAD] = LAYOUT_moonlander(
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, KC_GRV,  KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, KC_EQL,  KC_4,    KC_5,    KC_6,    KC_MINS, _______,
+        _______, _______, _______, _______, _______, _______,                             KC_BSLS, KC_1,    KC_2,    KC_3,    KC_SLSH, _______,
+        _______, _______, _______, _______, _______,          _______,           _______,          KC_0,    _______, _______, _______, _______,
+                                            _______, _______, _______,           _______, KC_LBRC, KC_RBRC
     ),
 
     [MDIA] = LAYOUT_moonlander(
